9e8ccef19c0f24cfb5ebc3ef6e2bd2905c5374c4
jose pedraza <pastorpedraza3@gmail.com>
2020-12-18 12:26:48 -0600

    refactor code and working with all features

--- index.js
+++ index.js
@@ -1,16 +1,12 @@
 const buttonColors = ['red','blue','green','yellow'];
 let gamePattern = [];
 let userPattern = [];
+let start = false;
+let level = 0;
+
 const randomNumber = () => {
   let x = Math.floor(Math.random()*4);
   return x;
-};
-
-const animPress = (tag) => {
-    $(`#${tag}`).addClass('pressed');
-    setTimeout(function(){
-        $(`#${tag}`).removeClass('pressed');
-    }, 100);
 };
 
 const sound = (input) => {
@@ -37,17 +33,52 @@ const sound = (input) => {
   }
 }
 
-$(document).keypress(function() {
+const animPress = (tag) => {
+    $(`#${tag}`).addClass('pressed');
+    setTimeout(function(){
+        $(`#${tag}`).removeClass('pressed');
+    }, 100);
+};
+
+const checkAnswer = (currentLevel) => {
+    let status = false
+    for ( let i = 0; i <= currentLevel; i += 1) {
+        if ( gamePattern[i] != userPattern[i] ) {
+          status = true;
+        };
+    };
+    console.log(status);
+    return status;
+}
+
+const nextSequence = () => {
+  console.log(level);
+  $('h1').text(`Leve ${level}`);
+  level += 1;
   let randomColor = buttonColors[randomNumber()];
+  gamePattern.push(randomColor);
   $(`#${randomColor}`).fadeOut(100).fadeIn(100).fadeOut(100).fadeIn(100);
   sound($(`#${randomColor}`)[0].id);
-  gamePattern.push(randomColor);
-});
+  userPattern = [];
+}
 
 $('.btn').click(function(e) {
   let userChoosencolor = e.target.id;
   userPattern.push(userChoosencolor);
   sound(userChoosencolor);
   animPress(userChoosencolor);
-  console.log(userPattern);
+  if ( gamePattern.length === userPattern.length) {
+    checkAnswer(level);
+    setTimeout(function(){
+      nextSequence();
+    }, 500);
+
+  }
+});
+
+$(document).keypress(function() {
+  if (start === false) {
+    nextSequence();
+  }
+  start = true;
 });
